2879abcfb00c053537b37fd33273b5b13fd14201
khalid0931 <mutiarif53@gmail.com>
2026-04-28 15:51:04 +0700

    membuat void traverse

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -129,3 +129,14 @@ public:
         delete current;
         cout << "Record with roll number " << rollNo << " deleted" << endl;
     } 
+
+    void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        // Step 1: Mark first node as currentNode
+        Node *currentNode = START;
